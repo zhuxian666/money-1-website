8ca3f9788d0dc7ca75392d14b597f103747d2f67
杨程 <7702269+yangcheng666@user.noreply.gitee.com>
2020-09-23 14:41:17 +0800

    deploy

---
+++ precache-manifest.00483552254f7c04d6d800a2ca41de55.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf5ab6f8e0f81139d54",
+    "url": "/money-1-website/css/app.e8a201c8.css"
+  },
+  {
+    "revision": "5fdfbfff58f9b9e1839f3bf04191b2a1",
+    "url": "/money-1-website/index.html"
+  },
+  {
+    "revision": "e8a410af9b121f99ba98",
+    "url": "/money-1-website/js/about.7438f7c6.js"
+  },
+  {
+    "revision": "baf5ab6f8e0f81139d54",
+    "url": "/money-1-website/js/app.aef25922.js"
+  },
+  {
+    "revision": "077d114942eaa67b377b",
+    "url": "/money-1-website/js/chunk-vendors.8ece185b.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-1-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-1-website/robots.txt"
+  }
+]);
